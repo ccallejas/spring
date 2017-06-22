0f396038d4f2b02e206062ae363fbf729028e53b
ccallejas <cleriacallejas@gmail.com>
2017-06-21 22:49:48 -0500

    comentarios en las interfaces

--- src/main/java/com/ccallejas/dao/DAOMarca.java
+++ src/main/java/com/ccallejas/dao/DAOMarca.java
@@ -2,6 +2,9 @@ package com.ccallejas.dao;
 
 import com.ccallejas.beans.Marca;
 
+/**@author Cleria
+ * interface
+ * */
 public interface DAOMarca {
 
 	public void registrar(Marca marca) throws Exception;
--- src/main/java/com/ccallejas/dao/DAOMarcaImpl.java
+++ src/main/java/com/ccallejas/dao/DAOMarcaImpl.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.ccallejas.beans.Marca;
 
+/**@author Cleria
+ * Clase
+ * */
 @Repository
 public class DAOMarcaImpl implements DAOMarca {
 	
